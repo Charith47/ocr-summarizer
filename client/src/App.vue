0663ffc188f054e99ad12d3cb314723ecb57c132
Charith <charithdissanayake47@gmail.com>
2022-01-06 00:17:34 +0530

    add::about,contributors,logo,favicon

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,25 +1,63 @@
+/* eslint-disable */
 <template>
 	<v-app id="inspire">
-		<v-app-bar app color="white" flat> </v-app-bar>
+		<v-app-bar app color="white" flat>
+			<v-container>
+				<img height="30px" width="auto" src="./assets/logo-main.svg" />
+			</v-container>
+		</v-app-bar>
 
 		<v-main class="grey lighten-3">
 			<v-container>
 				<v-row>
 					<v-col cols="12" sm="2">
 						<v-sheet rounded="lg" min-height="268">
-							<!--  -->
+							<v-container>
+								<div class="d-flex align-start">
+									<img
+										src="./assets/info-emoji.png"
+										height="25px"
+										width="auto"
+										alt="information emoji"
+									/>
+									<h3 class="mx-2">About</h3>
+								</div>
+								<p class="body-1">
+									Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem
+									corporis autem, pariatur mollitia maiores quidem nisi minima
+									maxime at, debitis eaque illo nulla amet? Minus officiis ut
+									quasi minima voluptatem!
+								</p>
+							</v-container>
 						</v-sheet>
 					</v-col>
 
 					<v-col cols="12" sm="8">
-						<v-sheet min-height="70vh" rounded="lg">
-							<!--  -->
-						</v-sheet>
+						<v-sheet min-height="70vh" rounded="lg"> </v-sheet>
 					</v-col>
 
 					<v-col cols="12" sm="2">
 						<v-sheet rounded="lg" min-height="268">
-							<!--  -->
+							<v-container>
+								<div class="d-flex align-start">
+									<img
+										src="./assets/tech-emoij.png"
+										height="25px"
+										width="auto"
+										alt="tech emoji"
+									/>
+									<h3 class="mx-2">Contributors</h3>
+								</div>
+
+
+								<v-list-item two-line>
+									<v-list-item-content>
+										<v-list-item-title>C.W Dissanayake</v-list-item-title>
+										<v-list-item-subtitle>D/BCE/20/0009</v-list-item-subtitle>
+									</v-list-item-content>
+								</v-list-item>
+
+							</v-container>
 						</v-sheet>
 					</v-col>
 				</v-row>
@@ -30,14 +68,13 @@
 				fab
 				href="https://github.com/Charith47/ocr-summarizer"
 				target="_blank"
-        color="secondary"
+				color="secondary"
 				bottom
 				right
 				fixed
 			>
 				<v-icon> mdi-github </v-icon>
 			</v-btn>
-
 		</v-main>
 	</v-app>
 </template>
