79a34955c4d8e571ec4e721866c4fa7f1c985207
Charith <charithdissanayake47@gmail.com>
2022-01-05 22:27:15 +0530

    add::client page layout

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,28 +1,51 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+	<v-app id="inspire">
+		<v-app-bar app color="white" flat> </v-app-bar>
+
+		<v-main class="grey lighten-3">
+			<v-container>
+				<v-row>
+					<v-col cols="12" sm="2">
+						<v-sheet rounded="lg" min-height="268">
+							<!--  -->
+						</v-sheet>
+					</v-col>
+
+					<v-col cols="12" sm="8">
+						<v-sheet min-height="70vh" rounded="lg">
+							<!--  -->
+						</v-sheet>
+					</v-col>
+
+					<v-col cols="12" sm="2">
+						<v-sheet rounded="lg" min-height="268">
+							<!--  -->
+						</v-sheet>
+					</v-col>
+				</v-row>
+			</v-container>
+
+			<!-- GitHub Link -->
+			<v-btn
+				fab
+				href="https://github.com/Charith47/ocr-summarizer"
+				target="_blank"
+        color="secondary"
+				bottom
+				right
+				fixed
+			>
+				<v-icon> mdi-github </v-icon>
+			</v-btn>
+
+		</v-main>
+	</v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
-  name: "App",
-  components: {
-    HelloWorld,
-  },
+	data: () => ({
+		focus: false,
+	}),
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
