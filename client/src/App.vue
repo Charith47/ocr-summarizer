4ac370d7a40ae2774b7831db77ee48fb9f988526
Charith <charithdissanayake47@gmail.com>
2022-01-08 18:32:25 +0530

    add:: frontend error handling and alerts. About and contrib changes

--- client/src/App.vue
+++ client/src/App.vue
@@ -7,8 +7,21 @@
 		</v-app-bar>
 
 		<v-main class="grey lighten-3">
+
+			<!-- error messages -->
+			<v-alert v-if="noFilesError" class="rounded-0" dense type="error">{{
+				noFilesError
+			}}</v-alert>
+			<v-alert v-if="apiError" class="rounded-0" dense type="error">{{
+				apiError
+			}}</v-alert>
+			<v-alert v-if="parseError" class="rounded-0" dense type="error">{{
+				parseError
+			}}</v-alert>
+
 			<v-container>
 				<v-row>
+					<!-- about section -->
 					<v-col cols="12" sm="2">
 						<v-sheet rounded="lg" min-height="268">
 							<v-container>
@@ -22,15 +35,17 @@
 									<h3 class="mx-2">About</h3>
 								</div>
 								<p class="body-1">
-									Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem
-									corporis autem, pariatur mollitia maiores quidem nisi minima
-									maxime at, debitis eaque illo nulla amet? Minus officiis ut
-									quasi minima voluptatem!
+									AccuText is as a text recognizer and a summarizer which can
+									extract and summarize text from uploaded images. Start by
+									uploading your images and clicking the upload button to get
+									the extracted text and click summarize button to get the
+									summarized text.
 								</p>
 							</v-container>
 						</v-sheet>
 					</v-col>
 
+					<!-- main app -->
 					<v-col cols="12" sm="8">
 						<v-sheet min-height="70vh" rounded="lg">
 							<v-container>
@@ -50,7 +65,7 @@
 											counter
 											multiple
 											truncate-length="15"
-											label="Images"
+											placeholder="Select images to upload"
 										></v-file-input>
 										<v-btn color="primary" @click="uploadFiles"> Upload </v-btn>
 									</v-container>
@@ -58,10 +73,14 @@
 
 								<v-container>
 									<v-container>
-										<h2 class="">Detected text</h2>
+										<h2 class="">Extracted text</h2>
 									</v-container>
 									<v-container>
-										<v-textarea outlined label="Detected test" :value="extractedText"></v-textarea>
+										<v-textarea
+											outlined
+											label="Extracted test"
+											:value="extractedText"
+										></v-textarea>
 										<v-btn color="primary"> Summarize </v-btn>
 									</v-container>
 								</v-container>
@@ -69,6 +88,7 @@
 						</v-sheet>
 					</v-col>
 
+					<!-- contributors -->
 					<v-col cols="12" sm="2">
 						<v-sheet rounded="lg" min-height="268">
 							<v-container>
@@ -82,17 +102,38 @@
 									<h3 class="mx-2">Contributors</h3>
 								</div>
 
-								<v-list-item two-line>
-									<v-list-item-content>
-										<v-list-item-title>C.W Dissanayake</v-list-item-title>
-										<v-list-item-subtitle>D/BCE/20/0009</v-list-item-subtitle>
-									</v-list-item-content>
-								</v-list-item>
+								<v-list>
+									<v-list-item
+										v-for="contributor in contributors"
+										:key="contributor.sid"
+										two-line
+									>
+										<v-list-item-content>
+											<v-list-item-title>
+												{{ contributor.name }}
+											</v-list-item-title>
+											<v-list-item-subtitle>
+												{{ contributor.sid }}
+											</v-list-item-subtitle>
+										</v-list-item-content>
+									</v-list-item>
+								</v-list>
 							</v-container>
 						</v-sheet>
 					</v-col>
 				</v-row>
 			</v-container>
+
+			<!-- uploading files -->
+			<v-snackbar v-model="snackbar">
+				{{ text }}
+
+				<template v-slot:action="{ attrs }">
+					<v-btn color="pink" text v-bind="attrs" @click="snackbar = false">
+						Close
+					</v-btn>
+				</template>
+			</v-snackbar>
 
 			<!-- GitHub Link -->
 			<v-btn
@@ -114,31 +155,80 @@
 import axios from 'axios';
 export default {
 	data: () => ({
+		// app data
 		parsedFiles: [],
-		extractedText: ""
+		extractedText: '',
+
+		// misc
+		contributors: [
+			{
+				name: 'C.W Dissanayake',
+				sid: 'D/BCE/20/0009',
+			},
+			{
+				name: 'T.W.L.A Perera',
+				sid: 'D/BCS/20/0010',
+			},
+			{
+				name: 'S.P Gayanika',
+				sid: 'D/BCE/20/0007',
+			},
+			{
+				name: 'R.W.B.M.P Nishadi',
+				sid: 'D/BSE/20/0018',
+			},
+			{
+				name: 'G.M.T Amarasinghe',
+				sid: 'D/BCS/20/00012',
+			},
+		],
+
+		// info messages
+		snackbar: true,
+		text: 'Hello',
+
+		// error messages
+		noFilesError: '',
+		parseError: '',
+		apiError: '',
 	}),
 	methods: {
 		parseFiles(files) {
+			// remove file errors
+			this.noFilesError = '';
+			this.parseError = '';
+
 			files.forEach(async (file) => {
-				const b64_string = await this.toBase64(file);
-				this.parsedFiles.push({
-					name: file.name,
-					image: b64_string.split(',')[1],
-				});
+				// handle parse error
+				try {
+					const b64_string = await this.toBase64(file);
+					this.parsedFiles.push({
+						name: file.name,
+						image: b64_string.split(',')[1],
+					});
+				} catch (error) {
+					this.parseError = `${error.message}`;
+				}
 			});
 		},
 		uploadFiles() {
+			// handle 0 files
+			if (this.parsedFiles.length === 0) {
+				this.noFilesError = 'No files are selected. Please select some images';
+				return;
+			}
+
 			axios
 				.post('http://127.0.0.1:3001/api/ocr/s/multi', this.parsedFiles)
 				.then((response) => {
-					console.log(response.data);
-					response.data.forEach(textObj=>{
+					response.data.forEach((textObj) => {
+						this.extractedText += textObj.name;
 						this.extractedText += textObj.text;
-						this.extractedText += "\n\n\n"
-					})
+						this.extractedText += '\n\n';
+					});
 				})
 				.catch((error) => {
-					console.log(error);
+					this.apiError = `${error.message}. Please try again later `;
 				});
 		},
 		toBase64(file) {
